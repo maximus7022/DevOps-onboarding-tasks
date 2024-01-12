d651c78a832c699c3bb59de08e3e538eec3b1289
maximus7022 <pash6790@gmail.com>
2024-01-12 17:08:16 +0200

    Updating s3 tf module

--- Lambda/modules/s3/main.tf
+++ Lambda/modules/s3/main.tf
@@ -1,8 +1,10 @@
+# \\\\\\\\\\S3 bucket creation//////////
 resource "aws_s3_bucket" "s3" {
   bucket        = var.bucket_name
   force_destroy = true
 }
 
+# \\\\\\\\\\Adding bucket versioning//////////
 resource "aws_s3_bucket_versioning" "s3_versioning" {
   bucket = aws_s3_bucket.s3.id
   versioning_configuration {
--- Lambda/modules/s3/outputs.tf
+++ Lambda/modules/s3/outputs.tf
@@ -1,3 +1,4 @@
+# \\\\\\\\\\Sharing bucket name with Lambda module//////////
 output "bucket_name" {
   value = aws_s3_bucket.s3.bucket
 }

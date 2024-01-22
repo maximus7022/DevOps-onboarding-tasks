be54746eadd0e1697fd22d701e22f03841b8926c
maximus7022 <pash6790@gmail.com>
2024-01-22 18:41:42 +0200

    Adding grafana module to root terraform folder

--- Lambda/main.tf
+++ Lambda/main.tf
@@ -1,11 +1,24 @@
 module "s3" {
-  source      = "./modules/s3"
-  bucket_name = var.bucket_name
+  source           = "./modules/s3"
+  bucket_name      = var.bucket_name
+  grafana_role_arn = module.grafana.grafana_role_arn
+  queue_name       = var.queue_name
 }
 
 module "lambda" {
   source           = "./modules/lambda"
-  role_name        = var.role_name
+  role_name        = var.lambda_role_name
   lambda_func_name = var.lambda_func_name
   s3_bucket        = module.s3.bucket_name
+}
+
+module "grafana" {
+  source            = "./modules/grafana"
+  instance_ami      = var.instance_ami
+  instance_type     = var.instance_type
+  instance_name_tag = var.instance_name_tag
+  role_name         = var.grafana_role_name
+  s3_bucket         = module.s3.bucket_name
+  profile_name      = var.profile_name
+  queue_arn         = module.s3.queue_arn
 }
--- Lambda/variables.tf
+++ Lambda/variables.tf
@@ -2,10 +2,34 @@ variable "bucket_name" {
   default = "s3-bucket-for-lmbd-output"
 }
 
-variable "role_name" {
+variable "lambda_role_name" {
   default = "LambdaRoleEC2S3"
 }
 
 variable "lambda_func_name" {
   default = "EC2healthCheckFunction"
+}
+
+variable "instance_ami" {
+  default = "ami-07ec4220c92589b40"
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "instance_name_tag" {
+  default = "grafana"
+}
+
+variable "grafana_role_name" {
+  default = "GrafanaRoleS3"
+}
+
+variable "profile_name" {
+  default = "grafana_iam_profile"
+}
+
+variable "queue_name" {
+  default = "s3-event-notification-queue"
 }
